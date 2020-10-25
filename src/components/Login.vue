04f6cc475c5a0194f421cfa77b16591312cf756a
Mykhailo Klymchuk <mishaklm139@gmail.com>
2020-10-25 11:59:00 +0200

    add scss and BEM technology

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="login-wrapper">
-        <div class="login-container">
-            <div class="login-header">
-                <h1 class="app-name login-name">Bookshelf</h1>
+    <div class="login">
+        <div class="login__wrapper">
+            <div class="login__header">
+                <h1 class="app-name login__name">Bookshelf</h1>
             </div>
-            <div class="login-body">
-                <img v-if="!login" class="login-icon" src="../assets/icon.png" alt="" />
+            <div class="login__body">
+                <img v-if="!login" class="login__icon" src="../assets/icon.png" alt="" />
             </div>
-            <div class="login-footer">
-                <button class="btn login-btn" @click="login = true">Get started</button>
+            <div class="login__footer">
+                <button class="btn login__btn" @click="login = true">Get started</button>
             </div>
         </div>
     </div>
@@ -29,14 +29,14 @@
     }
   }
   export default {
-    setup
+    setup,
   }
 </script>
 
-<style>
+<style lang="scss">
     @import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
 
-    .login-wrapper {
+    .login {
         position: fixed;
         top: 0;
         bottom: 0;
@@ -47,39 +47,49 @@
         justify-content: center;
         box-shadow: inset 0 0 550px #000000;
         background-image: url("../assets/login_background.jpg");
+        &__wrapper {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            margin: 10% 0;
+        }
+        &__icon {
+            width: 200px;
+        }
+        &__footer {
+            width: 100%;
+        }
+        &__btn {
+            margin: 0 auto;
+            display: block;
+        }
+        &__header {
+            display: flex;
+            width: 100%;
+        }
+        &__name {
+            margin: 0 auto;
+        }
     }
-    .login-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    }
-    .login-icon {
-        width: 100px;
-    }
-    .login-footer {
-        width: 100%;
-    }
-    .login-btn {
-        margin: 0 auto;
-        display: block;
-    }
+
     .btn {
-        background: #e29e32;
+        background: #e1a73b;
         border: none;
         border-radius: 30px;
         padding: 15px 59px;
         font-size: 22px;
         font-weight: 700;
         box-shadow: 0 0 550px #000000;
-    }
-    .btn:hover {
-        /*background: #ffa620;*/
-    }
-    .btn:active {
-        background: #ffa620;
-    }
-    .btn:focus {
-        outline: none;
+        &:hover {
+            /*background: #ffa620;*/
+        }
+        &:active {
+            background: #fbcc67;
+        }
+        &:focus {
+            outline: none;
+        }
     }
     .app-name {
         font-family: 'Quicksand', sans-serif;
@@ -87,12 +97,5 @@
         font-size: 75px;
         text-shadow: 0px 0px 0px black, 0 0 1em black;
         text-transform: uppercase;
-    }
-    .login-header {
-        display: flex;
-        width: 100%;
-    }
-    .login-name {
-        margin: 0 auto;
     }
 </style>
